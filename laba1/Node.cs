57ea6d305eff2420078b46806a65be69c6245f2d
zzuykin <145152148+zzuykin@users.noreply.github.com>
2024-03-09 23:28:33 +0300

    + 3 задание

---
+++ laba1/Node.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba1
+{
+    public class Node
+    {
+        public List<Node> child { get; set; }
+        public string value { get; set; }
+        public Node(string value) { 
+            this.value = value;
+            child = new List<Node>();
+        }
+        public void NewChild(Node node)
+        {
+            child.Add(node);
+        }
+        public void RemoveChild(Node node)
+        {
+            child.Remove(node);
+        }
+        public void ShowTree()
+        {
+            if(child.Count > 0)
+            {
+                Console.WriteLine($"Потомки узла {value} :");
+                for (int i = 0; i < child.Count; i++)
+                {
+                    Console.WriteLine(child[i].value);
+                }
+                for (int i = 0; i < child.Count; i++)
+                {
+                    child[i].ShowTree();
+                }
+            }
+        }
+    }
+}
--- laba1/Program.cs
+++ laba1/Program.cs
@@ -1,15 +1,48 @@
 ﻿using laba1;
 
-
-var item1 = new TimeEuro();
-Console.WriteLine(item1.ShowTime());
-
-var timeDecorator = new TimeEuroDecorator(item1);
-Console.WriteLine(timeDecorator.ShowTime());
+task3();
 
 
-var item2 = new TimeUSA();
-Console.WriteLine(item2.ShowTime());
+//задание 2
 
-var timeUSDEC = new TimeUSADecorator(item2);
-Console.WriteLine(timeUSDEC.ShowTime());
+void task2()
+{
+    var item1 = new TimeEuro();
+    Console.WriteLine(item1.ShowTime());
+
+    var timeDecorator = new TimeEuroDecorator(item1);
+    Console.WriteLine(timeDecorator.ShowTime());
+
+
+    var item2 = new TimeUSA();
+    Console.WriteLine(item2.ShowTime());
+
+    var timeUSDEC = new TimeUSADecorator(item2);
+    Console.WriteLine(timeUSDEC.ShowTime());
+}
+
+//задание 3
+
+void task3()
+{
+    Node begin = new Node("Ручка");
+    Node node1 = new Node("Колпачек");
+    Node node2 = new Node("Стержень");
+    Node node3 = new Node("Корпус");
+    Node node4 = new Node("Нижняя часть");
+    Node node5 = new Node("Верзняя часть");
+    Node node6 = new Node("Паста");
+    Node node7 = new Node("Трубочка");
+    Node node8 = new Node("Химикаты");
+
+    begin.NewChild(node1);
+    begin.NewChild(node2);
+    begin.NewChild(node3);
+    node3.NewChild(node4);
+    node3.NewChild(node5);
+    node2.NewChild(node6);
+    node2.NewChild(node7);
+    node6.NewChild(node8);
+
+    begin.ShowTree();
+}
